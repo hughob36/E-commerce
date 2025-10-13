58e2f4db68a52fc5bcac2f8989c5676e3bdb676b
Hugo Benitez <hugo.boca16@gmail.com>
2025-10-13 17:24:07 -0300

    User details service

--- e_commerce/src/main/java/com/e_commerce/service/UserDetailsServiseImpl.java
+++ e_commerce/src/main/java/com/e_commerce/service/UserDetailsServiseImpl.java
@@ -1,21 +1,50 @@
 package com.e_commerce.service;
 
 
+
+import com.e_commerce.model.Role;
+import com.e_commerce.model.UserApp;
+import com.e_commerce.repository.IUserAppRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDetailsServiseImpl implements UserDetailsService {
+
+    @Autowired
+    private IUserAppRepository userAppRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+           UserApp userApp = userAppRepository.findEntityUserAppByUsername(username)
+                   .orElseThrow(() -> new UsernameNotFoundException("User '" + username + "' was not found."));
 
 
+        List<SimpleGrantedAuthority>authorityList = new ArrayList<>();
 
+        userApp.getRoleSet()
+                .forEach(role -> authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role.getRole()))));
 
+        userApp.getRoleSet()
+                .stream()
+                .flatMap(role -> role.getPermissionSet().stream())
+                .forEach(permission -> authorityList.add(new SimpleGrantedAuthority(permission.getPermissionName())));
 
-
-
-        return null;
+        return new User(
+                userApp.getUsername(),
+                userApp.getPassword(),
+                userApp.isEnable(),
+                userApp.isAccountNotExpired(),
+                userApp.isCredentialNotExpired(),
+                userApp.isAccountNotLocked(),
+                authorityList
+                );
     }
 }
