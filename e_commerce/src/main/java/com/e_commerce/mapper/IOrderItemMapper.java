8f71fcb58232fd1c6d2d1cc0110db3c08342ed04
Hugo Benitez <hugo.boca16@gmail.com>
2026-05-01 17:01:34 -0300

    orderItem service save method

--- e_commerce/src/main/java/com/e_commerce/mapper/IOrderItemMapper.java
+++ e_commerce/src/main/java/com/e_commerce/mapper/IOrderItemMapper.java
@@ -2,8 +2,14 @@ package com.e_commerce.mapper;
 
 import com.e_commerce.dto.OrderItemRequestDTO;
 import com.e_commerce.dto.OrderItemResponseDTO;
+import com.e_commerce.dto.OrderResponseSimpleDTO;
+import com.e_commerce.dto.ProductResponseSimpleDTO;
+import com.e_commerce.model.Order;
 import com.e_commerce.model.OrderItem;
+import com.e_commerce.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import java.util.List;
 
@@ -14,5 +20,14 @@ public interface IOrderItemMapper {
 
     public OrderItemResponseDTO toOrderItemResponseDTO(OrderItem orderItem);
 
+    @Mapping(target = "order", ignore = true)
+    @Mapping(target = "product", ignore = true)
     public OrderItem toOrderItem(OrderItemRequestDTO orderItemRequestDTO);
+
+    @Mapping(target = "order", ignore = true)
+    @Mapping(target = "product", ignore = true)
+    public void updateOrderItemFromDTO(OrderItemRequestDTO orderItemRequestDTO, @MappingTarget OrderItem orderItem);
+
+    public OrderResponseSimpleDTO toOrderResponseSimpleDTO(Order order);
+    public ProductResponseSimpleDTO toProductResponseSimpleDTO(Product product);
 }
--- e_commerce/src/main/java/com/e_commerce/service/OrderItemService.java
+++ e_commerce/src/main/java/com/e_commerce/service/OrderItemService.java
@@ -2,7 +2,14 @@ package com.e_commerce.service;
 
 import com.e_commerce.dto.OrderItemRequestDTO;
 import com.e_commerce.dto.OrderItemResponseDTO;
+import com.e_commerce.exception.ResourceNotFoundException;
+import com.e_commerce.mapper.IOrderItemMapper;
+import com.e_commerce.model.Order;
+import com.e_commerce.model.OrderItem;
+import com.e_commerce.model.Product;
+import com.e_commerce.repository.IOrderItemRepository;
 import com.e_commerce.repository.IOrderRepository;
+import com.e_commerce.repository.IProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,30 +19,53 @@ import java.util.List;
 @RequiredArgsConstructor
 public class OrderItemService implements IOrderItemService{
 
+    private final IOrderItemRepository orderItemRepository;
+    private final IOrderItemMapper orderItemMapper;
     private final IOrderRepository orderRepository;
+    private final IProductRepository productRepository;
 
     @Override
     public List<OrderItemResponseDTO> findAll() {
-        return List.of();
+        List<OrderItem> orderItemList = orderItemRepository.findAll();
+        return orderItemMapper.toOrderItemResponseDTOList(orderItemList);
     }
 
     @Override
     public OrderItemResponseDTO findById(Long id) {
-        return null;
+        OrderItem orderItem = orderItemRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("OrderItem '"+ id +"' not found."));
+        return orderItemMapper.toOrderItemResponseDTO(orderItem);
     }
 
     @Override
     public OrderItemResponseDTO save(OrderItemRequestDTO orderItemRequestDTO) {
-        return null;
+
+        OrderItem orderItem = orderItemMapper.toOrderItem(orderItemRequestDTO);
+
+        Order foundOrder = orderRepository.findById(orderItemRequestDTO.getOrderId())
+                .orElseThrow(() -> new ResourceNotFoundException("Order '"+ orderItemRequestDTO.getOrderId() +"' not found."));
+        orderItem.setOrder(foundOrder);
+
+        Product foundProduct = productRepository.findById(orderItemRequestDTO.getProductId())
+                .orElseThrow(() -> new ResourceNotFoundException("Product '"+ orderItemRequestDTO.getProductId() +"' not found."));
+        orderItem.setProduct(foundProduct);
+
+        OrderItem savedOrderItem = orderItemRepository.save(orderItem);
+
+        return orderItemMapper.toOrderItemResponseDTO(savedOrderItem);
     }
 
     @Override
     public void deleteById(Long id) {
-
+        if(!orderItemRepository.existsById(id)) {
+            throw new ResourceNotFoundException("OrderItem '"+ id +"' not found.");
+        }
+        orderItemRepository.deleteById(id);
     }
 
     @Override
     public OrderItemResponseDTO updateById(Long id, OrderItemRequestDTO orderItemRequestDTO) {
+
         return null;
     }
 }
