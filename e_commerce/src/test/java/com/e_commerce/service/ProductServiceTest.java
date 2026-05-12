bade072ab1dc070e3a244700dc7863a1515195ce
Hugo Benitez <hugo.boca16@gmail.com>
2026-05-12 17:29:26 -0300

    productServiceTest findById and resourceNotFoundException

--- e_commerce/src/test/java/com/e_commerce/service/ProductServiceTest.java
+++ e_commerce/src/test/java/com/e_commerce/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package com.e_commerce.service;
 import com.e_commerce.dto.CategoryResponseSimpleDTO;
 import com.e_commerce.dto.ProductImageResponseDTO;
 import com.e_commerce.dto.ProductResponseDTO;
+import com.e_commerce.exception.ResourceNotFoundException;
 import com.e_commerce.mapper.IProductMapper;
 import com.e_commerce.model.Category;
 import com.e_commerce.model.Product;
@@ -18,10 +19,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
@@ -95,8 +96,105 @@ public class ProductServiceTest {
                 () -> assertEquals("product1", result.get(0).getName(), "The role name does not match")
         );
         verify(productRepository, times(1)).findAll();
-        verify(productMapper, times(1)).toProductResponseDTOList(anyList());
+        verify(productMapper, times(1)).toProductResponseDTOList(productList);
         verifyNoMoreInteractions(productRepository,productMapper);
+    }
+
+    @Test
+    @DisplayName("Should return an empty list when no products exist in the database")
+    public void findAll_ShouldReturnEmptyProductResponseDTOList() {
+
+        List<Product> productList = List.of();
+        List<ProductResponseDTO> productResponseDTOList = List.of();
+
+        when(productRepository.findAll()).thenReturn(productList);
+        when(productMapper.toProductResponseDTOList(productList)).thenReturn(productResponseDTOList);
+
+        List<ProductResponseDTO> result = productService.findAll();
+
+        assertAll("Verify empty response properties",
+                () -> assertNotNull(result, "The result should not be null even if empty"),
+                () -> assertTrue(result.isEmpty(), "The list should be empty"),
+                () -> assertEquals(0, result.size(), "The size of the list should be 0")
+        );
+        verify(productRepository, times(1)).findAll();
+        verify(productMapper, times(1)).toProductResponseDTOList(productList);
+    }
+
+    @Test
+    @DisplayName("Should return a ProductResponseDTO when findById is called")
+    public void findByID_ShouldProductResponseDTO() {
+
+        Long id = 1L;
+        Category category = Category.builder()
+                .id(1L)
+                .name("electric")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        ProductImage productImg = new ProductImage();
+        List<ProductImage> productImgList = List.of(productImg);
+
+        Product product = Product.builder()
+                .id(1L)
+                .name("product1")
+                .description("product prueba")
+                .price(new BigDecimal("100.00"))
+                .stock(10)
+                .sku("pru1")
+                .category(category)
+                .imageUrl("www.prueba.com")
+                .isActive(true)
+                .createdAt(LocalDateTime.now())
+                .images(productImgList)
+                .build();
+
+        CategoryResponseSimpleDTO categoryResponseSimpleDTO = new CategoryResponseSimpleDTO();
+        ProductImageResponseDTO productImgRes = new ProductImageResponseDTO();
+        List<ProductImageResponseDTO> productImageResponseDTOList = List.of(productImgRes);
+
+        ProductResponseDTO productResponseDTO = ProductResponseDTO.builder()
+                .id(1L)
+                .name("product1")
+                .description("product prueba")
+                .price(new BigDecimal("100.00"))
+                .stock(10)
+                .sku("pru1")
+                .category(categoryResponseSimpleDTO)
+                .imageUrl("www.prueba.com")
+                .isActive(true)
+                .createdAt(LocalDateTime.now())
+                .images(productImageResponseDTOList)
+                .build();
+
+        when(productRepository.findById(id)).thenReturn(Optional.of(product));
+        when(productMapper.toProductResponseDTO(product)).thenReturn(productResponseDTO);
+
+        ProductResponseDTO result = productService.findById(id);
+
+        assertAll("Verify response properties",
+                () -> assertNotNull(result, "The result should not be null"),
+                () -> assertEquals("product1", result.getName(), "The product name does not match"),
+                () -> assertEquals(id, result.getId(), "The ID does not match")
+        );
+        verify(productRepository, times(1)).findById(id);
+        verify(productMapper, times(1)).toProductResponseDTO(product);
+        verifyNoMoreInteractions(productRepository,productMapper);
+    }
+
+    @Test
+    @DisplayName("Should throw ResourceNotFoundException when ID does not exist")
+    public void findByID_ShouldThrowResourceNotFoundException_WhenIdDoesNotExist() {
+
+        Long id = 1L;
+        when(productRepository.findById(id)).thenReturn(Optional.empty());
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class,
+                () -> productService.findById(id));
+
+        assertEquals("Product '1' not found.", exception.getMessage());
+        verify(productRepository, times(1)).findById(id);
+        verifyNoInteractions(productMapper);
     }
 
 }
