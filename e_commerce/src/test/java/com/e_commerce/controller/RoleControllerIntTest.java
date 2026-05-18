3414637c9d74e9de12e5541d85853e08c9a83a30
Hugo Benitez <hugo.boca16@gmail.com>
2026-05-18 17:27:50 -0300

    test

--- e_commerce/src/test/java/com/e_commerce/controller/RoleControllerIntTest.java
+++ e_commerce/src/test/java/com/e_commerce/controller/RoleControllerIntTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class RoleControllerIntTest {
 
     @Autowired
