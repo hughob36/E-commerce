d404d4c6d32f8d7745247fe8fd43a500972ed41c
Hugo Benitez <hugo.boca16@gmail.com>
2026-06-01 19:01:19 -0300

    properties

--- e_commerce/src/main/java/com/e_commerce/model/Category.java
+++ e_commerce/src/main/java/com/e_commerce/model/Category.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Table(name = "categories")
